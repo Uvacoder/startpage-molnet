570617796b76bee3df7f3ae41fc285a72aa89baa
madakralc <adam.clark91@gmail.com>
2021-05-17 15:27:47 +0200

    Back to white

--- assets/js/themer.js
+++ assets/js/themer.js
@@ -138,7 +138,7 @@ for (let i = 0; i < dataThemeButtons.length; i++) {
                 setTheme({
                     'color-background': '#366164',
                     'color-text-pri': '#53BBAD',
-                    'color-text-acc': '#AEEA00'
+                    'color-text-acc': '#EEEEEE'
                 });
                 return;
 
